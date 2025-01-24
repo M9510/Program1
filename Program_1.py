69bbf9b8e90970f12025b4f7875363d55f5015ef
Moksha Suthar <Mokshasuthar9@gmail.com>
2025-01-24 12:55:57 +0530

    Create Program_1.py

---
+++ Program_1.py
@@ -0,0 +1,82 @@
+# Arithmetic program
+
+a = 10
+b = 5
+
+print(a + b)
+
+a = 10
+b = 5
+
+print(a - b)
+
+a = 10
+b = 5
+
+print(a * b)
+
+a = 10
+b = 5
+
+print(a / b)
+
+a = 10
+b = 5
+
+print(a % b)
+
+# Area of circle program
+
+r=9
+a=3.14*r
+print(a)
+
+# comparison program
+
+x = 15
+y = 20
+
+print(x > y)
+
+x = 15
+y = 20
+
+print(x < y)
+
+x = 15
+y = 20
+
+print(x == y)
+
+x = 15
+y = 20
+
+print(x != y)
+
+x = 15
+y = 20
+
+print(x >= 10 and y <= 25)
+
+# Swape program
+
+a=9
+b=8
+b=a
+
+print(b)
+a=b
+print(a)
+
+# simple intrest calculations
+
+# Declare variables
+principal = 5000
+rate = 5
+time = 2
+
+# Calculate simple interest
+simple_interest = (principal * rate * time) / 100
+
+# Print the simple interest
+print("The Simple Interest is:", simple_interest)
